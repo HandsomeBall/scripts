6460f3fd6029dc891f1affc37be3a4ee2a7d250a
HandsomeBall <3152149160@qq.com>
2015-11-15 21:58:52 +0800

    centos7 repo 替换脚本

---
+++ centos7-repo-163.sh
@@ -0,0 +1,10 @@
+# 首先备份/etc/yum.repos.d/CentOS-Base.repo
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+# 进入yum源配置文件所在文件夹
+cd /etc/yum.repos.d/
+# 下载163的yum源配置文件，放入/etc/yum.repos.d/(操作前请做好相应备份)
+wget http://mirrors.163.com/.help/CentOS6-Base-163.repo
+# 运行yum makecache生成缓存
+yum makecache
+# 更新系统
+yum -y update
